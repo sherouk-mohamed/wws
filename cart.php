30f18c0495d8268d88632b68c822b47882ce9d85
sherouk <sheroukmohamed246@gmail.com>
2025-05-16 13:36:09 +0300

    Add files via upload

---
+++ cart.php
@@ -0,0 +1,11 @@
+<?php 
+    require_once ("Product.php");
+
+ class Cart
+{
+  
+private SplDoublyLinkedList $products ;
+private float $totalAmount;
+
+}
+?>
---
+++ product.php
@@ -0,0 +1,18 @@
+<?php 
+    require_once ("admin.php");
+
+ class product
+{
+  
+private string $productID;
+public  float $price;
+public string $name;
+private int $stockQuantity;
+public string $pDetails;
+
+    public function updateStock($productID, $stockQuantity) {
+
+    }
+
+}
+?>
