cd068d07b23d355a3ad07aa5a3a34eb977fa36aa
JannaEmad008 <jannaemad008@gmail.com>
2025-05-16 15:09:10 +0300

    Add files via upload

---
+++ admin.php
@@ -0,0 +1,52 @@
+<?php 
+require_once("user.php");
+require_once("interface.php");
+interface Websuser {
+    public function getId(): string; 
+}
+
+class teacher extends User implements Webuser {
+    public function __construct($userID, $name, $email, $address, $encryptedPassword, $role, $phoneNumber) {
+        parent::__construct($userID, $name, $email, $address, $encryptedPassword, $role, $phoneNumber);
+    }
+
+    public function getId(): string {
+        return $this->getUserID();
+    }
+}
+
+
+class parent extends User implements Webuser {
+    public function __construct($userID, $name, $email, $address, $encryptedPassword, $role, $phoneNumber) {
+        parent::__construct($userID, $name, $email, $address, $encryptedPassword, $role, $phoneNumber);
+    }
+
+    public function getId(): string {
+        return $this->getUserID();
+    }
+}
+
+
+class WebusertNode {
+    public Webuser $data;
+    public ?WebuserNode $next = null;
+
+    public function __construct(Webuser $Webuser) {
+        $this->data = $Webuser ;
+    }
+}
+class admin extends user {
+    public ?Webuser $head = null; 
+    function generateQRCode(?Webuser $head)
+    {
+
+    }
+    function approverequest() {
+
+    }
+    function rejectrequest()
+    {
+        
+    }
+}
+?>
