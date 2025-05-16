d8d5331ea9fe2b0d5ef957ffa37d919b3d9b5846
sherouk <sheroukmohamed246@gmail.com>
2025-05-16 13:07:59 +0300

    Add files via upload

---
+++ ScanQRCode.php
@@ -0,0 +1,27 @@
+<?php 
+require_once ("teacher.php");
+class notification 
+{
+private string $scanID;
+public string $timestamp;
+public string $childID;
+public string $activityType;
+
+    public function ScanQRCode($childID) {
+
+    }    
+    public function logActivity(){
+    }   
+     public function logParentVerification() {
+    }  
+      public function logMeal() {
+    }     
+     public function logAttendance() {
+
+    }     
+     public function logNap() {
+        
+    }
+
+}
+?>
