6bee79869ff2e42d755735d13e5616bcafd932d5
JannaEmad008 <jannaemad008@gmail.com>
2025-05-16 18:14:43 +0300

    Update feepayment.php

--- feepayment.php
+++ feepayment.php
@@ -4,7 +4,29 @@
 private float $feeAmount;
 private string $dueDate;
 private string $parentID;
+ public function getFeeAmount(): float {
+        return $this->feeAmount;
+    }
+    public function setFeeAmount(float $feeAmount): void {
+        $this->feeAmount = $feeAmount;
+    }
 
+   
+    public function getDueDate(): string {
+        return $this->dueDate;
+    }
+    public function setDueDate(string $dueDate): void {
+        $this->dueDate = $dueDate;
+    }
+
+  
+    public function getParentID(): string {
+        return $this->parentID;
+    }
+    public function setParentID(string $parentID): void {
+        $this->parentID = $parentID;
+    }
+}
 
 }
 ?>
