5099d98be673f1e2782081289cf2e2b6f74a7672
JannaEmad008 <jannaemad008@gmail.com>
2025-05-16 15:08:16 +0300

    Add files via upload

---
+++ teacher.php
@@ -0,0 +1,16 @@
+<?php
+required_once("user")
+required_once("homework")
+class teacher extends user{
+    public $assignedClass;
+    public $classStudents;
+    function viewClassSchedule()
+    {
+
+    }
+    function gradeHomework($homework)
+    {
+        
+    }
+}
+?>
